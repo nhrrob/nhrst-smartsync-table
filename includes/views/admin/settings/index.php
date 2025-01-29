1bff35066ac239d23ab1ec9a349674f25f8df310
Nazmul Hasan Robin <robin.sust08@gmail.com>
2025-01-29 11:56:27 +0600

    table design

--- includes/views/admin/settings/index.php
+++ includes/views/admin/settings/index.php
@@ -15,19 +15,39 @@ $rows = $data['data']['rows'] ?? [];
 ?>
 
 <div class="wrap">
-    <div id="nhrst-admin-settings">NHR SmartSync Table</div>
-    
-    <h1><?php echo esc_html($data['title'] ?? 'API Data'); ?></h1>
+    <div class="nhrst-smartsync-table-settings-page">
+        <div class="nhrst-settings-page-navbar">
+            <?php 
+            $table_url = add_query_arg([
+                'page' => $this->page_slug,
+                '_wpnonce' => wp_create_nonce('nhrst-settings-nonce')
+            ], admin_url('tools.php'));
+            
+            $settings_url = add_query_arg([
+                'page' => $this->page_slug,
+                'tab' => 'settings',
+                '_wpnonce' => wp_create_nonce('nhrst-settings-nonce')
+            ], admin_url('tools.php'));
+            ?>
+            
+            <a href="<?php echo esc_url($table_url); ?>" class="tab active">
+                NHR SmartSync Table
+            </a>
+            
+        </div>
+        
+        <h1><?php echo esc_html($data['title'] ?? 'API Data'); ?></h1>
 
-    <button id="refresh-data">Refresh Data</button>
-    
+        <p class="nhrst-table-refresh-button-wrap"><button id="refresh-data" class="nhrst-button nhrst-table-refresh-button">Refresh Data</button></p>
+        
 
-    <form action="" method="post">
-        <?php
-        $table = new SettingsTable($data);
-        $table->prepare_items();
-        // $table->search_box( 'search', 'search_id' );
-        $table->display();
-        ?>
-    </form>
+        <form class="nhrst-settings-page-form" action="" method="post">
+            <?php
+            $table = new SettingsTable($data);
+            $table->prepare_items();
+            // $table->search_box( 'search', 'search_id' );
+            $table->display();
+            ?>
+        </form>
+    </div>
 </div>
